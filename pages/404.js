4ce8733c0adf5bf581733dfce3840baa411c53e9
Thiago Peixoto <thiago.peixoto@hotmart.com>
2023-02-20 18:38:20 -0300

    Finished Next.js course

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link'
+
+export default function Page404() {
+  return (
+    <div>
+      <h1>404</h1>
+      <Link href="/">Ir para a home</Link>
+    </div>
+  )
+}
---
+++ pages/faq.js
@@ -0,0 +1,18 @@
+import FAQScreen from '../src/screens/FAQScreen';
+
+export default FAQScreen;
+
+// export async function getServerSideProps()
+export async function getStaticProps() {
+  const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+
+  const faq = await fetch(FAQ_API_URL)
+    .then(response => response.json())
+    .then(data => data)
+
+  return {
+    props: {
+      faq
+    },
+  }
+}
